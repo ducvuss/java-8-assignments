1933a81114182a45b82579802087dd24918bf6ba
Duc Vu <duc.vu@smoothstack.com>
2020-04-22 12:59:41 -0500

    added more date time api examples

--- src/exercise/datetime/DateTimeDemo.java
+++ src/exercise/datetime/DateTimeDemo.java
@@ -1,11 +1,14 @@
 package exercise.datetime;
 
 import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 import com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray;
@@ -19,6 +22,51 @@ public class DateTimeDemo {
 		demo.previousThursday();
 		demo.workWithZoneID();
 		demo.workWithZoneOffset();
+		demo.isFriday13();
+		demo.findAllMondaysInMonth();
+		demo.findMonthsLengthInYear();
+		demo.workWithInstant();
+	}
+	
+	
+
+	private void findMonthsLengthInYear() {
+
+		LocalDate date = LocalDate.now();
+		System.out.println("Lenght of each month of " + date.getYear());
+		IntStream
+		.rangeClosed(1, 12)
+		.forEach(month -> {
+			LocalDate checked = LocalDate.of(date.getYear(), month, date.getDayOfMonth());
+			
+			System.out.println(checked.getMonth() + ": " + checked.lengthOfMonth());
+		});
+	}
+
+
+
+	private void workWithInstant() {
+
+		Instant instant = Instant.now();
+		System.out.println("Instant is "+ instant);
+		
+		System.out.println(instant.atZone(ZoneId.of("Asia/Saigon")));
+		
+		ZonedDateTime datetime = ZonedDateTime.now();
+		System.out.println(datetime.toInstant());
+	}
+
+
+
+	private void findAllMondaysInMonth() {
+
+		LocalDate date = LocalDate.now();
+		
+		System.out.println("Every Moday of this month: ");
+		IntStream.rangeClosed(1, date.lengthOfMonth()).filter(day -> {
+			LocalDate checked = LocalDate.of(date.getYear(), date.getMonth(), day);
+			return (checked.getDayOfWeek() == DayOfWeek.MONDAY);
+		}).forEach(day -> System.out.println(LocalDate.of(date.getYear(), date.getMonth(), day)));
 	}
 
 	public void storeBirthday() {
@@ -58,10 +106,28 @@ public class DateTimeDemo {
 		ZoneId zone = ZoneId.of("Asia/Saigon");
 
 		System.out.println(zone + " now is " + date.now(zone));
-		
 
 		ZoneOffset zoneOffSet = zone.getRules().getOffset(date);
 		System.out.println("time offset is " + zoneOffSet);
+
+	}
+
+	public void isFriday13() {
+		LocalDate date = LocalDate.now();
+
+		if (date.getDayOfMonth() == 13 && date.getDayOfWeek() == DayOfWeek.FRIDAY) {
+			System.out.println(date + " is " + "Firday the 13th");
+		} else {
+			System.out.println(date + " is not " + "Firday the 13th");
+		}
+
+		LocalDateTime badDay = LocalDateTime.of(2019, Month.DECEMBER, 13, 0, 0, 0);
+
+		if (badDay.getDayOfMonth() == 13 && badDay.getDayOfWeek() == DayOfWeek.FRIDAY) {
+			System.out.println(badDay + " is " + "Firday the 13th");
+		} else {
+			System.out.println(badDay + " is not " + "Firday the 13th");
+		}
 
 	}
 
