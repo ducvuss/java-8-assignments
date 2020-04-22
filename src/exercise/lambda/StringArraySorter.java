fa5b65d3db0d29c32927f64472b3ab1518c05248
Duc Vu <duc.vu@smoothstack.com>
2020-04-22 10:16:06 -0500

    refactored codes

--- src/exercise/lambda/StringArraySorter.java
+++ src/exercise/lambda/StringArraySorter.java
@@ -4,42 +4,42 @@ import java.util.Arrays;
 
 public class StringArraySorter {
 	public static void main(String[] args) {
-		String[] strings = {"abcd", "b", "bcd", "xy", "zabcde", "n", "omn"};
-		
+		String[] strings = { "abcd", "b", "bcd", "xy", "zabcde", "n", "omn" };
+
 		StringArraySorter sorter = new StringArraySorter();
 		sorter.sortByFirstChar(strings);
 	}
-	
+
 	public String[] sortArray(String[] strings) {
-		if(strings.length < 2) {
+		if (strings.length < 2) {
 			return strings;
 		}
-		
+
 		Arrays.sort(strings, (string1, string2) -> string1.length() - string2.length());
 		return strings;
 	}
 
 	public String[] reverseSortArray(String[] strings) {
-		if(strings.length < 2) {
+		if (strings.length < 2) {
 			return strings;
 		}
-		
+
 		Arrays.sort(strings, (string1, string2) -> string2.length() - string1.length());
 		return strings;
 	}
-	
+
 	public String[] sortByFirstChar(String[] strings) {
-		if(strings.length < 2) {
+		if (strings.length < 2) {
 			return strings;
 		}
-		
+
 		Arrays.sort(strings, (string1, string2) -> string1.charAt(0) - string2.charAt(0));
 //		Arrays.stream(strings).forEach(System.out::println);
 		return strings;
 	}
 
 	public String[] sortByMiddleChar(String[] strings) {
-		if(strings.length < 2) {
+		if (strings.length < 2) {
 			return strings;
 		}
 		Arrays.sort(strings, (string1, string2) -> {
@@ -51,9 +51,9 @@ public class StringArraySorter {
 //		Arrays.stream(strings).forEach(System.out::println);
 		return strings;
 	}
-	
+
 	public String[] sortByMiddleCharWithStaticHelper(String[] strings) {
-		if(strings.length < 2) {
+		if (strings.length < 2) {
 			return strings;
 		}
 		Arrays.sort(strings, Utils::comparesByMiddleChar);
@@ -69,5 +69,3 @@ interface Utils {
 		return string2ContainsE - string1ContainsE;
 	}
 }
-
-
--- src/exercise/lambda/test/StringArraySorterTest.java
+++ src/exercise/lambda/test/StringArraySorterTest.java
@@ -2,7 +2,6 @@ package exercise.lambda.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 import org.junit.jupiter.api.Test;
 
 import exercise.lambda.StringArraySorter;
@@ -10,60 +9,60 @@ import exercise.lambda.StringArraySorter;
 public class StringArraySorterTest {
 	@Test
 	public void sortArrayTest() {
-		String[] strings = {"abcd", "a", "abc", "ab", "abcde", "a", "ab"};
-		String[] sorted = {"a", "a", "ab", "ab", "abc", "abcd", "abcde",};
-		
+		String[] strings = { "abcd", "a", "abc", "ab", "abcde", "a", "ab" };
+		String[] sorted = { "a", "a", "ab", "ab", "abc", "abcd", "abcde", };
+
 		StringArraySorter sorter = new StringArraySorter();
 		String[] sortedArr = sorter.sortArray(strings);
-		for(int i = 0; i < strings.length; i++) {
+		for (int i = 0; i < strings.length; i++) {
 			assertEquals(sorted[i], sortedArr[i]);
 		}
 	}
-	
+
 	@Test
 	public void reverseSortArrayTest() {
-		String[] strings = {"abcd", "a", "abc", "ab", "abcde", "a", "ab"};
-		String[] sorted = {"abcde", "abcd", "abc", "ab", "ab", "a", "a"};
-		
+		String[] strings = { "abcd", "a", "abc", "ab", "abcde", "a", "ab" };
+		String[] sorted = { "abcde", "abcd", "abc", "ab", "ab", "a", "a" };
+
 		StringArraySorter sorter = new StringArraySorter();
 		String[] sortedArr = sorter.reverseSortArray(strings);
-		for(int i = 0; i < strings.length; i++) {
+		for (int i = 0; i < strings.length; i++) {
 			assertEquals(sorted[i], sortedArr[i]);
 		}
 	}
-	
+
 	@Test
 	public void sortByFirstCharTest() {
-		String[] strings = {"abcd", "b", "bcd", "xy", "zabcde", "n", "omn"};
-		String[] sorted = {"abcd", "b", "bcd", "n", "omn", "xy", "zabcde"};
-		
+		String[] strings = { "abcd", "b", "bcd", "xy", "zabcde", "n", "omn" };
+		String[] sorted = { "abcd", "b", "bcd", "n", "omn", "xy", "zabcde" };
+
 		StringArraySorter sorter = new StringArraySorter();
 		String[] sortedArr = sorter.sortByFirstChar(strings);
-		for(int i = 0; i < strings.length; i++) {
+		for (int i = 0; i < strings.length; i++) {
 			assertEquals(sorted[i], sortedArr[i]);
 		}
 	}
-	
+
 	@Test
 	public void sortByMiddleCharTest() {
-		String[] strings = {"abcd", "be", "bcd", "xye", "zabcde", "n", "omn"};
-		String[] sorted = {"be", "xye", "zabcde", "abcd", "bcd", "n", "omn"};
-		
+		String[] strings = { "abcd", "be", "bcd", "xye", "zabcde", "n", "omn" };
+		String[] sorted = { "be", "xye", "zabcde", "abcd", "bcd", "n", "omn" };
+
 		StringArraySorter sorter = new StringArraySorter();
 		String[] sortedArr = sorter.sortByMiddleChar(strings);
-		for(int i = 0; i < strings.length; i++) {
+		for (int i = 0; i < strings.length; i++) {
 			assertEquals(sorted[i], sortedArr[i]);
 		}
 	}
-	
+
 	@Test
 	public void sortByMiddleCharWithStaticHelperTest() {
-		String[] strings = {"abcd", "be", "bcd", "xye", "zabcde", "n", "omn"};
-		String[] sorted = {"be", "xye", "zabcde", "abcd", "bcd", "n", "omn"};
-		
+		String[] strings = { "abcd", "be", "bcd", "xye", "zabcde", "n", "omn" };
+		String[] sorted = { "be", "xye", "zabcde", "abcd", "bcd", "n", "omn" };
+
 		StringArraySorter sorter = new StringArraySorter();
 		String[] sortedArr = sorter.sortByMiddleCharWithStaticHelper(strings);
-		for(int i = 0; i < strings.length; i++) {
+		for (int i = 0; i < strings.length; i++) {
 			assertEquals(sorted[i], sortedArr[i]);
 		}
 	}
